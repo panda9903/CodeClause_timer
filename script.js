e6e92dab51d4805c91d536843ac348e93313b737
panda9903 <satwik9903@gmail.com>
2023-05-30 20:23:18 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let breakTime = 5;
 
 let seconds = "00"
 
-// display
 window.onload = () => {
     document.getElementById('minutes').innerHTML = workTime;
     document.getElementById('seconds').innerHTML = seconds;
@@ -16,13 +15,10 @@ window.onload = () => {
     workTittle.classList.add('active');
 }
 
-// start timer
 function start() {
-    // change button
     document.getElementById('start').style.display = "none";
     document.getElementById('reset').style.display = "block";
 
-    // change the time
     seconds = 59;
 
     let workMinutes = workTime - 1;
@@ -30,13 +26,10 @@ function start() {
 
     breakCount = 0;
 
-    // countdown
     let timerFunction = () => {
-        //change the display
         document.getElementById('minutes').innerHTML = workMinutes;
         document.getElementById('seconds').innerHTML = seconds;
 
-        // start
         seconds = seconds - 1;
 
         if(seconds < 10){
@@ -55,19 +48,15 @@ function start() {
             workMinutes = workMinutes - 1;
             if(workMinutes === -1 ){
                 if(breakCount % 2 === 0) {
-                    // start break
                     workMinutes = breakMinutes;
                     breakCount++
 
-                    // change the painel
                     workTittle.classList.remove('active');
                     breakTittle.classList.add('active');
                 }else {
-                    // continue work
                     workMinutes = workTime;
                     breakCount++
 
-                    // change the painel
                     breakTittle.classList.remove('active');
                     workTittle.classList.add('active');
                 }
@@ -76,6 +65,5 @@ function start() {
         }
     }
 
-    // start countdown
-    setInterval(timerFunction, 1000); // 1000 = 1s
+    setInterval(timerFunction, 1000); 
 }
